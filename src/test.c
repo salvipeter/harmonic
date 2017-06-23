02bb92a2ff037396ecc8807352e7d4688e26a5d9
Peter Salvi <vukung@yahoo.com>
2017-06-23 15:50:48 +0200

    Non-recursive fixed-memory flood-fill algorithm.

--- src/test.c
+++ src/test.c
@@ -1,18 +1,24 @@
 #include "harmonic.h"
 
 int main(int argc, char **argv) {
-  double input[] = {0, 0, 0,
-                    6, 0, 0,
-                    6, 6, 0,
-                    4, 6, 0,
-                    4, 2, 0,
-                    2, 2, 0,
-                    2, 6, 1,
-                    0, 6, 1};
-  struct HarmonicMap *map = harmonic_init(8, input, 9, 1.0e-5);
+  double input[] = { 2, 0, 1,
+                     4, 1, 1,
+                     6, 0, 0,
+                     7, 2, 0,
+                     5, 3, 0,
+                     6, 4, 0,
+                     7, 7, 0,
+                     5, 5, 0,
+                     4, 7, 0,
+                     2, 5, 0,
+                     0, 7, 0,
+                     1, 4, 0,
+                     3, 3, 0,
+                     1, 2, 0};
+  struct HarmonicMap *map = harmonic_init(14, input, 9, 1.0e-5);
   double point[] = { 0.5, 0.5 };
   harmonic_eval(map, point);
-  harmonic_write_ppm(map, "/tmp/u.ppm");
+  harmonic_write_ppm(map, "/tmp/csillag.ppm");
   harmonic_free(map);
   return 0;
 }
