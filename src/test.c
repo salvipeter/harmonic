cfdb76699b7166c6fa842e570349b9e69b13221f
Peter Salvi <vukung@yahoo.com>
2018-02-03 23:23:09 +0100

    Added mesh generation example.

--- src/test.c
+++ src/test.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "harmonic.h"
 
@@ -17,12 +18,35 @@ int main(int argc, char **argv) {
   struct HarmonicMap *map;
 
   map = harmonic_init(7, input, levels, 1.0e-5);
+
+  /* Evaluation test */
   success = harmonic_eval(map, point, &result);
   if (success)
     printf("%lf\n", result);
   else
     printf("N/A\n");
+
+  /* PPM output test */
   harmonic_write_ppm(map, "/tmp/test.ppm");
+
+  /* Mesh generation test */
+
+  unsigned int v_size = harmonic_mesh_size(map, 2);
+  double *v = (double *)malloc(sizeof(double) * v_size * 2);
+  unsigned int *t = (unsigned int *)malloc(sizeof(unsigned int) * v_size * 6);
+  unsigned int t_size = harmonic_mesh(map, 2, v, t);
+
+  FILE *f = fopen("/tmp/test.obj", "w");
+  for (unsigned int i = 0; i < v_size; ++i)
+    fprintf(f, "v %f %f 0.0\n", v[2*i], v[2*i+1]);
+  for (unsigned int i = 0; i < t_size; ++i)
+    fprintf(f, "f %d %d %d\n", t[3*i] + 1, t[3*i+1] + 1, t[3*i+2] + 1);
+  fclose(f);
+
+  free(t);
+  free(v);
+
   harmonic_free(map);
+
   return 0;
 }
